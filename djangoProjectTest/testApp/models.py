e836ade9f7b4e7e33f376ddd08b5ae437739d3d8
ipsot <mort8@yandex.ru>
2022-08-25 13:23:45 +0300

    DjangoProject

--- djangoProjectTest/testApp/models.py
+++ djangoProjectTest/testApp/models.py
@@ -4,3 +4,41 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
+
+
+def create():
+    tom = Person.objects.create(name="Tom", age=23)
+
+
+def save():
+    tim = Person(name="Bob", age=32)
+    tim.save()
+
+
+def get():
+    bob = Person.objects.get(age=23)
+    bob=Person.save()
+
+
+def getall():
+    people = Person.objects.all()
+
+
+def filter():
+    people = Person.objects.filter(age=32)
+
+
+def in_blunk():
+    people = Person.objects.in_bulk([1, 3])
+    for id in people:
+        print(people[id].name)
+        print(people[id].age)
+
+
+def filterAndUpdate():
+    pip=Person.objects.filter(id=1).update(name="Bobby")
+
+
+def delete():
+    dot=Person.objects.get(id=2)
+    dot.delete()
